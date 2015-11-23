6f921d4d4e9918df4eb3211d5cd3f0b1d4b003f9
losborne <losborne@bu.edu>
2015-11-22 22:39:09 -0500

    initial commit

---
+++ app/src/main/java/teoth/ec327/therektangle/Game.java
@@ -0,0 +1,57 @@
+package teoth.ec327.therektangle;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Game extends Activity {
+
+    static MediaPlayer soundtrack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // don't display the title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+
+        setContentView(new GamePanel(this));
+        soundtrack = MediaPlayer.create(this, R.raw.total_eclipse_of_the_heart);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_game, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static void playMusic(View view)
+    {
+        soundtrack.start();
+    }
+}
