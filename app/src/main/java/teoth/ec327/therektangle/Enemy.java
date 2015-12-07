ff9e45c004794dc86f3cf2e5137c5124927defe4
SamiSousa <dr.wisper@gmail.com>
2015-12-06 19:03:31 -0500

    added new xml layout file for menu, added onClick function to Game class

--- app/src/main/java/teoth/ec327/therektangle/Enemy.java
+++ app/src/main/java/teoth/ec327/therektangle/Enemy.java
@@ -10,13 +10,13 @@ import java.util.Random;
  * Created by Luke on 12/5/2015.
  */
 public class Enemy extends GameObject {
-    private Animation animation = new Animation();
-    private Bitmap sprites;
-    private int score; // used to make enemies faster as game progresses;
-    private int v; // velocity
-    private Random rand = new Random();
-    private long startTime;
-    private char side; // which side the enemy comes from
+    protected Animation animation = new Animation();
+    protected Bitmap sprites;
+    protected int score; // used to make enemies faster as game progresses;
+    protected int v; // velocity
+    protected Random rand = new Random();
+    protected long startTime;
+    protected char side; // which side the enemy comes from
 
     public Enemy(Bitmap b, int w, int h, char side, int s, int x, int y, int v, int nFrames)
     {
@@ -41,6 +41,13 @@ public class Enemy extends GameObject {
                 image[i] = Bitmap.createBitmap(sprites, i * width, 0, width, height);
             }
         }
+        //for sinusoidal enemies
+        if(side == 's'){
+            //move from left to right
+            for (int i = 0; i < image.length; i++) {
+                image[i] = Bitmap.createBitmap(sprites, 0, i * height, width, height);
+            }
+        }
         // don't make them TOO fast
         if (this.v >= 42){this.v = 42;}
 
@@ -49,7 +56,38 @@ public class Enemy extends GameObject {
 
         animation.setFrames(image);
         animation.setDelay(100-v); // change based on how fast they are
+    }
+
+    //finding the sine-velocity
+    public float sin_velocity(int velocity){
+        //returns a float for the curve velocity of the sinusoidal motion
+        //based on screen position and velocity of enemy
+        //might add frequency/period for tighter sinusoids; for now a constant frequency
+
+        //max sin-velocity at start and for this.y == start_y (left/ right)
+
+        if(side == 'l' || side == 'r'){
+            if(this.x % 500 < 250 ){
+                //top half of sine
+                return (float) (this.v*((1.0/100.0)*(this.x % 500) - (1.0/25000.0)*(this.x % 500)*(this.x % 500)));
+            }else{
+                //bottom half of sine
+                return (float) ((-1.0)*this.v*((1.0/100.0)*(this.x % 250) - (1.0/25000.0)*(this.x % 250)*(this.x % 250)));
+
             }
+        }else if(side == 't'|| side == 'b'){
+            if(this.y % 500 < 250){
+                //top half of sine
+                return (float) (this.v*((1.0/200.0)*(this.y % 500) - (1.0/25000.0)*(this.y % 500)*(this.y % 500)));
+            }else{
+                //bottom half of sine
+                return (float) ((-1.0)*this.v*((1.0/200.0)*(this.y % 250) - (1.0/25000.0)*(this.y % 250)*(this.y % 250)));
+
+            }
+        }
+        return 0;
+    }
+
     public void update()
     {
         // velocities will be appropriate, set in constructor
@@ -66,6 +104,11 @@ public class Enemy extends GameObject {
                 break;
             case 'b':
                 this.y += v;
+                break;
+            case 's':
+                //for sinusoidal enemies
+                this.x += v;
+                this.y += sin_velocity(v);
                 break;
             default:
                 break;
